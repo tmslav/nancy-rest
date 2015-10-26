2881cd9323e8670bd09e7da4e02e7b576ed219eb
Tomislav R <tmslav@gmail.com>
2015-10-26 10:39:40 +0100

    code

---
+++ Data.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testAPI.data
+{
+	public class Data{
+		public List<DataUser> users = new List<DataUser>();
+		public List<DataPost>posts = new List<DataPost>();
+
+		public void InsertUser(int user_id,string user_name,string user_password){
+			users.Insert (0, new DataUser{ user_id=user_id, user_name=user_name, user_password=user_password });
+		}
+
+		public DataUser GetUserById(int id){
+			foreach (DataUser u in users)
+				if (u.user_id == id)
+					return u;
+			return null;
+		}
+				
+		public bool RemoveUser(int user_id){
+			DataUser u = GetUserById (user_id);
+			if (u != null) {
+				users.Remove (u);
+				return true;
+			}
+			return false;
+			
+		}
+
+		public void InsertPost(int post_id,string post_body,string post_user){
+			if (posts.Count > post_id)
+				posts.Insert (post_id, new DataPost{ post_id = post_id, post_body = post_body, post_user = post_user });
+			else {
+				posts.Insert (0, new DataPost{ post_id = post_id, post_body = post_body, post_user = post_user });
+			}
+		}
+
+		public DataPost GetPostById(int id){
+			foreach (DataPost p in posts)
+				if (p.post_id == id)
+					return p;
+			return null;
+		}
+
+		public bool RemovePost(int post_id){
+			DataPost p = GetPostById (post_id);
+			if (p != null) {
+				posts.Remove (p);
+				return true;
+			}
+			return false;
+		}
+	}
+
+	public class DataUser
+	{
+		public int user_id{ get; set; }
+		public string user_name{ get; set; }
+		public string user_password{ get; set; }
+	}
+
+	public class DataPost{
+		public int post_id{ get; set; }
+		public string post_body{ get; set; }
+		public string post_user{ get; set; }
+	}
+}
+
---
+++ testAPI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Nancy;
+using testAPI.data;
+using System.Web.Script.Serialization;
+using Nancy.Extensions;
+
+namespace testAPI.Models
+{
+	public class testAPI:NancyModule
+	{
+		private static Data data = new Data();
+		public testAPI ()
+		{
+			Get["/"] = _ => "REST Server";
+
+			//POSTS
+			Get ["/posts/{id}"] = p => {
+				return new JavaScriptSerializer().Serialize(data.GetPostById(p.id));
+			};
+
+			Post ["/posts/new/"] = p => {
+				DataPost dp = new JavaScriptSerializer().Deserialize<DataPost>(this.Request.Body.AsString());
+				data.InsertPost(dp.post_id,dp.post_body,dp.post_user);
+				return Response.AsRedirect("/posts/"+dp.post_id.ToString(),Nancy.Responses.RedirectResponse.RedirectType.SeeOther);
+			};
+
+			Delete ["/posts/delete/{id}"] = p => {
+				return data.RemovePost (p.id);
+			};
+
+			Get["/posts/list"] = _=>{
+				return new JavaScriptSerializer().Serialize(data.posts);
+			};
+
+
+			//USERS
+			Get ["/users/{id}"] = p => {
+				return new JavaScriptSerializer().Serialize(data.GetPostById(p.id));
+			};
+
+			Post ["/users/new/"] = p => {
+				DataUser dp = new JavaScriptSerializer().Deserialize<DataUser>(this.Request.Body.AsString());
+				data.InsertUser(dp.user_id,dp.user_name,dp.user_password);
+				return Response.AsRedirect("/users/"+dp.user_id.ToString(),Nancy.Responses.RedirectResponse.RedirectType.SeeOther);
+			};
+			Delete["/users/delete/{id}"] = p =>{
+				return data.RemoveUser(p.id);
+			};
+
+			Get ["/users/list"] = p => {
+				return new JavaScriptSerializer ().Serialize (data.users);
+
+			};
+		}
+	}
+}
